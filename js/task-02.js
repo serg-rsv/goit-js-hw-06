b51c6edcf1a8f0bc4fddfe0b3405c8bd29dfcbe1
Serhii Rybak <rybaksw@gmail.com>
2022-03-11 11:25:24 +0200

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   "Herbs",
   "Condiments",
 ];
+
+const listRef = document.body.querySelector("#ingredients");
+
+function makeIngredientsList(items) {
+  return items.map((item) => {
+    const itemEl = document.createElement("li");
+    itemEl.classList.add("item");
+    itemEl.textContent = item;
+    return itemEl;
+  });
+}
+
+listRef.append(...makeIngredientsList(ingredients));
