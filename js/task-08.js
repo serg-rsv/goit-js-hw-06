0e35c362dc8aa16c348a5b22f8fd7588dfac7f1b
Serhii Rybak <rybaksw@gmail.com>
2022-03-14 17:35:36 +0200

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.currentTarget);
+  const entries = {};
+
+  formData.forEach((value, name) => (entries[name] = value));
+
+  const isEmptyField = Object.values(entries).includes("");
+
+  if (isEmptyField) {
+    alert("Все поля должны быть заполнены.");
+  } else {
+    console.log(entries);
+    event.currentTarget.reset();
+  }
+}
