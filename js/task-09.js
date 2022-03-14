4d2c28beba48c495c73a8ab10bf4f64267b27ab5
Serhii Rybak <rybaksw@gmail.com>
2022-03-14 18:59:43 +0200

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const colorEl = document.querySelector(".color");
+const btnColorEl = document.querySelector(".change-color");
+
+btnColorEl, addEventListener("click", onBtnClick);
+
+function onBtnClick() {
+  const color = getRandomHexColor();
+
+  colorEl.textContent = color;
+  document.body.style.backgroundColor = color;
 }
