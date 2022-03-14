d4bde2bcf8ad4682c0acf45c73012a2fc1bf7921
Serhii Rybak <rybaksw@gmail.com>
2022-03-14 20:05:02 +0200

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,49 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  input: document.querySelector("div > input"),
+  btnCreate: document.querySelector("[data-create]"),
+  btnDestroy: document.querySelector("[data-destroy]"),
+  boxWrapper: document.querySelector("#boxes"),
+};
+
+function createBoxes(amount) {
+  let boxSize = 30;
+  const arrOfBoxes = [];
+
+  for (let i = 1; i <= amount; i++) {
+    const size = `${boxSize}px`;
+    const color = getRandomHexColor();
+
+    arrOfBoxes.push(createBox(size, color));
+    boxSize += 10;
+  }
+
+  refs.boxWrapper.append(...arrOfBoxes);
+}
+
+function createBox(size, color) {
+  const divEl = document.createElement("div");
+  divEl.style.width = size;
+  divEl.style.height = size;
+  divEl.style.backgroundColor = color;
+
+  return divEl;
+}
+
+function destroyBoxes() {
+  refs.boxWrapper.innerHTML = "";
+}
+
+refs.btnCreate.addEventListener("click", onCreateClick);
+refs.btnDestroy.addEventListener("click", onDestroyClick);
+
+function onCreateClick() {
+  createBoxes(refs.input.value);
+}
+
+function onDestroyClick() {
+  destroyBoxes();
+}
